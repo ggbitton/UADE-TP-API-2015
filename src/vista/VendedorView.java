2632ebd27392993794f22d549eff9f1db6c31e1b
arielantog <arielantog@gmail.com>
2015-05-02 11:35:02 -0300

    Emitir resumen

---
+++ src/vista/VendedorView.java
@@ -0,0 +1,62 @@
+package vista;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import modelo.ItemColocacion;
+
+public class VendedorView {
+	private int nroVendedor;
+	private String direccion;
+	private String zona;
+	private int tipo;
+	private Vector<ItemColocacion> itemsColocaciones;
+	
+	public VendedorView(){
+	
+	}
+
+	public int getNroVendedor() {
+		return nroVendedor;
+	}
+
+	public void setNroVendedor(int nroVendedor) {
+		this.nroVendedor = nroVendedor;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getZona() {
+		return zona;
+	}
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public Vector<ItemColocacion> getItemsColocaciones() {
+		return itemsColocaciones;
+	}
+
+	public void setItemsColocaciones() {
+		itemsColocaciones = new Vector<ItemColocacion>();
+	}
+
+	public void agregarItem (ItemColocacion item){
+		itemsColocaciones.add(item);
+	}
+}
